51d21b73c08f32504e5ffe8529375db4d40d8c11
Murali <murali.24.4.91@gmail.com>
2018-09-24 23:21:09 -0500

    Added machine modules

--- Code/modules/machine/machinerouter.js
+++ Code/modules/machine/machinerouter.js
@@ -1,26 +1,68 @@
-const express = require('express');
-const router = express.Router();
-const User = require('../../models/user/user');
-const passport = require('passport');
-const jwt = require('jsonwebtoken');
-const config = require('../../config/database');
+var express = require('express');
+var router = express.Router();
 
-router.post("/register",(req, res, next)=>{
-    let newUser = new User({
-        firstName : req.body.firstName,
-        lastName : req.body.lastName,
-        phone : req.body.phone,
-        email : req.body.email,
-        username : req.body.username,
-        password : req.body.password
+var Machine = require('../../models/machine');
+var session = require('express-session');
+//retriving todos
+router.get('/todos/:email', (req, res, next) => {
+    console.log(req.user);
+    req.session.email=req.params.email;
+    Machine.getMachines(req.params.email,(err, todos) => {
+        res.json(todos);
     });
-    User.addUser(newUser, (err, user) =>{
-        if(err){
-            res.json({success : false, msg : "Failed to register user"});
-        }else{
-            res.json({success : true, msg : "User registered"});
+    //res.send('Redirected to Contant list');
+});
+
+//add machine
+router.post('/machine', (req, res, next) => {
+    //logic to add machine
+    let newTodo = new Machine({
+        email: req.body.email,
+        task: req.body.task,
+        isCompleted: req.body.isCompleted,
+        isEditing: req.body.isEditing
+    });
+    Machine.addTodo(newTodo, (err, machine) => {
+        if (err) {
+            res.json({ msg: 'Failed while adding new contact', status: 'error' });
+        } else {
+            // res.json({ msg: 'new contact added successfully' });
+           Machine.getMachines(machine.email,(err, todos) => {
+                res.json(todos);
+           });
         }
     });
+});
+
+//update machine
+router.put('/machine/:id', function (req, res,next) {
+    var id = req.params.id;
+    var update = (req.body.task === undefined) ? { isCompleted : req.body.isCompleted }:{ task: req.body.task };
+    Machine.updateTodo(id, update, (err, machine) => {
+         if (err) {
+            res.json({ msg: 'Failed while updating contact', status: 'error' });
+        } else {
+            // res.json({ msg: 'new contact added successfully' });
+           Machine.getMachines(machine.email,(err, todos) => {
+                res.json(todos);
+           });
+        }
+    });
+});
+
+//delete machine
+router.delete('/machine/:id', (req, res, next) => {
+    //logic to delete machine
+    Machine.deleteTodo(req.params.id,(err, result) => {
+        if (err) {
+            res.json({ msg: 'Failed while deleting contact', status: 'error',success:false });
+        } else {
+           
+            Machine.getMachines(req.session.email,(err, todos) => {
+                    res.json(todos);
+            });
+        }
+    })
 });
 
 module.exports = router;
