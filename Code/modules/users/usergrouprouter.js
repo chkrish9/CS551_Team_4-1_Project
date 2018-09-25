c21f4528fda5625c5f41c395044b5a9bc1c738f8
kaphc <kaphc@mail.umkc.edu>
2018-09-24 22:50:31 -0500

    added other routes in usergrouprouter

--- Code/modules/users/usergrouprouter.js
+++ Code/modules/users/usergrouprouter.js
@@ -5,7 +5,7 @@ const passport = require('passport');
 const jwt = require('jsonwebtoken');
 const config = require('../../config/database');
 
-router.post("/register",(req, res, next)=>{
+router.post("/add",(req, res, next)=>{
     let newUser = new User({
         firstName : req.body.firstName,
         lastName : req.body.lastName,
@@ -23,4 +23,39 @@ router.post("/register",(req, res, next)=>{
     });
 });
 
+router.post("/delete",(req, res, next)=>{
+    let newUser = new User({
+        firstName : req.body.firstName,
+        lastName : req.body.lastName,
+        phone : req.body.phone,
+        email : req.body.email,
+        username : req.body.username,
+        password : req.body.password
+    });
+    User.addUser(newUser, (err, user) =>{
+        if(err){
+            res.json({success : false, msg : "Failed to register user"});
+        }else{
+            res.json({success : true, msg : "User registered"});
+        }
+    });
+});
+ 
+router.post("/update",(req, res, next)=>{
+    let newUser = new User({
+        firstName : req.body.firstName,
+        lastName : req.body.lastName,
+        phone : req.body.phone,
+        email : req.body.email,
+        username : req.body.username,
+        password : req.body.password
+    });
+    User.addUser(newUser, (err, user) =>{
+        if(err){
+            res.json({success : false, msg : "Failed to register user"});
+        }else{
+            res.json({success : true, msg : "User registered"});
+        }
+    });
+});
 module.exports = router;
