b69cd31fe75db3e5a31647d77919f9b8662208d2
Murali <murali.24.4.91@gmail.com>
2018-09-25 22:35:45 -0500

    added machine route

--- Code/routes/routes.js
+++ Code/routes/routes.js
@@ -1,5 +1,7 @@
 var users = require('../modules/users/userrouter');
+var machines = require('../modules/machine/machinerouter');
 
 module.exports = function router(app){
     app.use('/users',users);
+    app.use('/machine',machines);
 };
