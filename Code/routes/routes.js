12a79b2b7885db5ac88394587a5b5c05cad4d719
rahulyerva <42656632+rahulyerva@users.noreply.github.com>
2018-09-25 22:44:48 -0500

    Added Route

--- Code/routes/routes.js
+++ Code/routes/routes.js
@@ -1,5 +1,6 @@
 var users = require('../modules/users/userrouter');
-
+var login = require('../modules/users/loginrouter');
 module.exports = function router(app){
     app.use('/users',users);
+    app.use('/login',login);
 };
