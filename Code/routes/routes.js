20d103f7ac575c829bf45b8ed5d6d3f7aaa49c93
RupeshDoddala <rdcb3@mail.umkc.edu>
2018-09-26 23:15:01 -0500

    change in router

--- Code/routes/routes.js
+++ Code/routes/routes.js
@@ -1,8 +1,10 @@
 var users = require('../modules/users/userrouter');
+var users = require('../modules/users/linerouter');
 var usergroups = require('../modules/users/usergrouprouter');
 var login = require('../modules/users/loginrouter');
 module.exports = function router(app){
     app.use('/users',users);
     app.use('/login',login);
     app.use('/usergroup',usergroups);
+	app.use('/line',linerouter);
 };
