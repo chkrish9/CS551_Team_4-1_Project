47232b241bc0ad9ce8e1adccab05e27da656e56e
kaphc <kaphc@mail.umkc.edu>
2018-09-26 23:51:53 -0500

    added router for area in routes

--- Code/routes/routes.js
+++ Code/routes/routes.js
@@ -1,8 +1,11 @@
 var users = require('../modules/users/userrouter');
 var usergroups = require('../modules/users/usergrouprouter');
 var login = require('../modules/users/loginrouter');
+var area = require('../modules/users/arearouter');
+
 module.exports = function router(app){
     app.use('/users',users);
     app.use('/login',login);
     app.use('/usergroup',usergroups);
+    app.use('/area',area);
 };
