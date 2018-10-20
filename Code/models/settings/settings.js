130f9fb2459ef2c46af67bcf8a92902bc1b08594
rahulyerva <42656632+rahulyerva@users.noreply.github.com>
2018-10-20 16:20:56 -0500

    updated settings.js in models

---
+++ Code/models/settings/settings.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+//Import schema
+const ImportModel = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+});
+
+const Import = module.exports = mongoose.model('ImportModel', ImportModel);
+
+module.exports.getImportById = function (id, callback) {
+    Import.findById(id, callback);
+}
+
+module.exports.addImport = function (newImport, callback) {
+    //console.log(newLine);
+    newLine.save(callback);
+}
+
+module.exports.updateImport = function (id, updateQuery, callback) {
+    Import.findByIdAndUpdate(id, { $set: updateQuery }, callback);
+}
+
+module.exports.deleteImport = function (id, callback) {
+    Import.remove({ _id: id }, callback);
+}
+
+module.exports.getImports = function (callback) {
+    Import.find().exec(callback);
+}
