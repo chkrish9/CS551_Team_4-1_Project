d23de92973bf960c86e15819348f6f22f5ffc7f0
rahulyerva <42656632+rahulyerva@users.noreply.github.com>
2018-10-22 23:37:11 -0500

    updated code in modules

--- Code/modules/settings/settingsrouter.js
+++ Code/modules/settings/settingsrouter.js
@@ -5,20 +5,7 @@ const Import = require('../../models/settings/import');
 const Settings = require('../../models/settings/import')
 
 
-//Create
-router.post('/create',passport.authenticate('jwt',{session : false}),  (req, res, next) =>{
-    let newLine = new Line({
-        name : req.body.name,
-        description : req.body.description
-    });
-    Import.addLine(newLine, (err, line) =>{
-        if(err){
-            res.json({success : false, msg : "Failed to Add user."});
-        }else{
-            res.json({success : true, msg : "User Added."});
-        }
-    });
-});
+
 
 
 
