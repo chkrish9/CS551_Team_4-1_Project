99a114ac734a4a045cfbf186d923782a6f4da22f
Tejaswi789 <42660107+Tejaswi789@users.noreply.github.com>
2018-10-24 08:35:54 -0500

    update parts

---
+++ Code/models/machine/parts.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+//Machine Group schema
+const partsModel = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+});
+
+const parts = module.exports = mongoose.model('partsModel',partsModel);
+
+module.exports.getpartsById = function (id, callback) {
+parts.findById(id, callback);
+}
+
+module.exports.addparts = function (newparts, callback) {
+    //console.log(newparts);
+    newparts.save(callback);
+}
+
+module.exports.updateparts = function (id, updateQuery, callback) {
+    parts.findByIdAndUpdate(id, { $set: updateQuery }, callback);
+}
+
+module.exports.deleteparts = function (id, callback) {
+    parts.remove({ _id: id }, callback);
+}
+
+module.exports.getparts = function (callback) {
+    parts.find().exec(callback);
+}
