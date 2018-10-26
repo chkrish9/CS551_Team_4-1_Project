fb38a7738d845e69f627a2885e975e894e72e016
rahulyerva <42656632+rahulyerva@users.noreply.github.com>
2018-10-25 23:51:47 -0500

    update

--- Code/models/settings/settings.js
+++ Code/models/settings/settings.js
@@ -18,6 +18,17 @@ module.exports.getImportById = function (id, callback) {
     Import.findById(id, callback);
 }
 
+module.exports.updateImport = function (id, updateQuery, callback) {
+    Import.findByIdAndUpdate(id, { $set: updateQuery }, callback);
+}
+
+module.exports.deleteImport = function (id, callback) {
+    Import.remove({ _id: id }, callback);
+}
+
+module.exports.getImports = function (callback) {
+    Import.find().exec(callback);
+}
 
 
 
