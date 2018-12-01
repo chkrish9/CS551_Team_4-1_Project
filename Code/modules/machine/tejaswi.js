c2be1ea6634276c8c6e1bb3f9afec25cd6edf798
Tejaswi789 <42660107+Tejaswi789@users.noreply.github.com>
2018-11-30 20:07:12 -0600

    update

--- Code/modules/machine/tejaswi.js
+++ Code/modules/machine/tejaswi.js
@@ -122,3 +122,54 @@ router.put('/update/:id',passport.authenticate('jwt',{session : false}),  functi
         }
     });
 });
+
+router.post('/create',passport.authenticate('jwt',{session : false}),  (req, res, next) =>{
+    let newPart = new Part({
+        name : req.body.name,
+        description : req.body.description,
+        isSerial: req.body.isSerial,
+        qrCode : req.body.qrCode,
+        availableQuantity:req.body.availableQuantity
+    });
+    newPart.qrCode = "{ partName :" + newPart.name +"}";
+    Part.addPart(newPart, (err, reason) =>{
+        if(err){
+            res.json({success : false, msg : "Failed to Add part."+err});
+        }else{
+            res.json({success : true, msg : "Part Added."});
+        }
+    });
+});
+
+//Update
+router.put('/update/:id',passport.authenticate('jwt',{session : false}),  function (req, res,next) {
+    var id = req.params.id;
+    var update = { 
+        name : req.body.name,
+        description : req.body.description,
+        isSerial: req.body.isSerial,
+        qrCode : req.body.qrCode,
+        availableQuantity:req.body.availableQuantity
+    };
+    update.qrCode = "{ partName :" + update.name + "}"
+    Part.updatePart(id, update, (err, reason) => {
+         if (err) {
+            res.json({ msg: 'Failed while updating part', status: 'error' });
+        } else {
+            res.json({ msg: 'new part updated successfully' });
+        }
+    });
+});
+
+//Delete
+router.delete('/delete/:id',passport.authenticate('jwt',{session : false}),  (req, res, next) => {
+    Part.deletePart(req.params.id,(err, result) => {
+        if (err) {
+            res.json({ msg: 'Failed while deleting part', status: 'error',success:false });
+        } else {
+            res.json({ msg: 'part deleted successfully' });
+        }
+    })
+});
+
+module.exports = router;
