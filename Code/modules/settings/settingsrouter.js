991de9ec6140fe5f57cd5b7a33e3fc2a4f0bdca1
rahulyerva <42656632+rahulyerva@users.noreply.github.com>
2018-11-30 21:20:36 -0600

    Commit_8

--- Code/modules/settings/settingsrouter.js
+++ Code/modules/settings/settingsrouter.js
@@ -10,20 +10,4 @@ router.get('/all',passport.authenticate('jwt', { session: false }), (req, res, n
     });
 });
 
-//Update
-router.put('/update/:id',passport.authenticate('jwt', { session: false }),  function (req, res, next) {
-    console.log(req.body);
-    var id = req.params.id;
-    var update = {
-        name: req.body.name,
-        value: req.body.value
-    };
-    Settings.updateSetting(id, update, (err, machineGroup) => {
-        if (err) {
-            res.json({ msg: 'Failed to update', status: 'error' });
-        } else {
-            res.json({ msg: 'updated successfully' });
-        }
-    });
-});
 
