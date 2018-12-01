eea0b480835b055f6612de1b105d3687f05c56d7
rahulyerva <42656632+rahulyerva@users.noreply.github.com>
2018-11-30 21:22:48 -0600

    Commit_9

--- Code/modules/settings/settingsrouter.js
+++ Code/modules/settings/settingsrouter.js
@@ -10,4 +10,35 @@ router.get('/all',passport.authenticate('jwt', { session: false }), (req, res, n
     });
 });
 
+//Update
+router.put('/update/:id',passport.authenticate('jwt', { session: false }),  function (req, res, next) {
+    console.log(req.body);
+    var id = req.params.id;
+    var update = {
+        name: req.body.name,
+        value: req.body.value
+    };
+    Settings.updateSetting(id, update, (err, machineGroup) => {
+        if (err) {
+            res.json({ msg: 'Failed to update', status: 'error' });
+        } else {
+            res.json({ msg: 'updated successfully' });
+        }
+    });
+});
 
+//Create
+router.post('/create',passport.authenticate('jwt', { session: false }), (req, res, next) =>{
+    let newSettings = new Settings({
+        name : req.body.name,
+        value : req.body.value
+    });
+    Settings.addSetting(newSettings, (err, Template) =>{
+        if(err){
+            res.json({success : false, msg : "Failed to Add new Settings."});
+        }else{
+            res.json({success : true, msg : "new Settings Added."});
+        }
+    });
+});
+module.exports = router;
