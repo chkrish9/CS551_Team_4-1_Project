2d020bc3e2ac59faccf0170c0bcbe1a9baccb4ac
Tejaswi789 <42660107+Tejaswi789@users.noreply.github.com>
2018-11-30 20:12:24 -0600

    update

--- Code/models/machine/partss.js
+++ Code/models/machine/partss.js
@@ -89,3 +89,32 @@ module.exports.getMachineGroups = function (callback) {
 }
 
 
+module.exports.getLineById = function (id, callback) {
+    Line.findById(id, callback);
+}
+
+module.exports.getLineByName = function (name, callback) {
+    Line.find({name:name},callback);
+}
+
+
+module.exports.updateLine = function (id, updateQuery, callback) {
+    Line.findByIdAndUpdate(id, { $set: updateQuery }, callback);
+}
+module.exports.addLine = function (newLine, callback) {
+    //console.log(newLine);
+    newLine.save(callback);
+}
+
+module.exports.deleteLine = function (id, callback) {
+    Line.remove({ _id: id }, callback);
+}
+
+module.exports.getLines = function (callback) {
+    Line.find().exec(callback);
+}
+
+module.exports.getLineNames = function(name,callback){
+    //console.log(name);
+    Line.find({ "name": { $regex: '.*' + name + '.*' }}, callback);
+}
